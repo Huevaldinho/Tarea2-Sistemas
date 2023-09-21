fa4e6faa139c6b6a918b231845f603cb0f086291
Felipe Obando <47650141+Huevaldinho@users.noreply.github.com>
2023-09-21 10:36:29 -0600

    Creacion archivo ejercicio 2

---
+++ Ejercicio2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h> 
+
+//Tarea 2
+/* EJERCICIO 2
+    Cree un proceso que cree a su vez 10 hilos hijos los cuáles deben generar
+     un número aleatorio (menor a 255) que imprimirán y luego retornarán al hilo 
+     padre como su estado de terminación. El hilo padre también debe imprimir
+      los números aleatorios que le retornaron sus hilos hijos al finalizar.
+*/
+int main() {
+
+    return 0;
+}
