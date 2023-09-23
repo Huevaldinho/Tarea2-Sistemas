e0cee29be608a7ac3abf0aaceb3709fb1c90b8f0
Felipe <felipeobando@estudiantec.cr>
2023-09-22 20:22:11 -0600

    Ejercicio 3

--- Tarea2_3-2021035489.c
+++ Tarea2_3-2021035489.c
@@ -14,53 +14,59 @@
     una de las posiciones del arreglo, y un arreglo de mutexes 
     para sincronizar los diferentes hilos.
 */
-int array[fiboAcalcular];
-pthread_mutex_t arrayMutex[fiboAcalcular];
+int array[fiboAcalcular];//Almacena los valores de Fibonacci
+pthread_mutex_t arrayMutex[fiboAcalcular];//Sincroniadores
 
-void * fibonacci(void *arg) {
-    int n = *((int *)arg);
+//Calcula fibonacci del num.
+//Tiene mucha repeticion de calculo por usar recursion.
+void * fibonacci(void * num){
+    //Saca el numero que envia el padre al crear el hilo
+    int n = *((int *)num);
+    int siguienteFibo = n+1;
 
-    if (n <= 1) {
+    if (n>fiboAcalcular)//Termina porque ya calculó el Fibo(n)
+        pthread_exit(NULL);
+    if (n <= 1) {//Fibo(0)=0 & Fibo(1)=1
         array[n] = n;
-    } else {
+    } else {//Resto de Fibo
         int a, b;
-        
-        pthread_mutex_lock(&arrayMutex[n - 1]);
-        a = array[n - 1];
-        pthread_mutex_unlock(&arrayMutex[n - 1]);
-
-        pthread_mutex_lock(&arrayMutex[n - 2]);
-        b = array[n - 2];
-        pthread_mutex_unlock(&arrayMutex[n - 2]);
-
-        array[n] = a + b;
+        //Se bloquean las posiciones para sincronizar los procesos 
+        //y asi no se modifique al mismo tiempo
+        //Fibo n = Fibo n-1 + Fibo n-2 
+        pthread_mutex_lock(&arrayMutex[n - 1]);//Bloquea pos de Fibo n-1
+        a = array[n - 1];//Toma fibo de n-1
+        pthread_mutex_unlock(&arrayMutex[n - 1]);//Desbloquea pos de Fibo n-1
+        pthread_mutex_lock(&arrayMutex[n - 2]);//Bloquea pos de Fibo n-2
+        b = array[n - 2];//Toma fibo de n-2
+        pthread_mutex_unlock(&arrayMutex[n - 2]);//Desbloquea pos de Fibo n-2
+        array[n] = a + b;//Calcula Fibo de n
     }
-
-    pthread_exit(NULL);
+    //* Descomentar para ver el calculo de Fibo hecho por cada hilo.
+    //printf("Hilo: %lu calculó Fibo(%d) = %d\n", (unsigned long)pthread_self(),n,array[n]);
+    fibonacci(&(siguienteFibo));
 }
 
 int main() {
     pthread_t threads[NUM_THREADS];
     int thread_args[NUM_THREADS];
-
-    for (int i = 0; i < 30; i++) {
+    //Inicializa el array de mutex 
+    for (int i = 0; i <= fiboAcalcular; i++) {
         pthread_mutex_init(&arrayMutex[i], NULL);
     }
-
+    //Crea los 5 hilos, manda a calcular Fibo de 0 a 4, los demas 
+    //se mandan a calcular dentro de la ejcucion de la funcion fibonacci.
     for (int i = 0; i < NUM_THREADS; i++) {
-        thread_args[i] = i + 2;  // Iniciar los hilos desde la posición 2
+        thread_args[i] = i;
         pthread_create(&threads[i], NULL, fibonacci, &thread_args[i]);
     }
-
+    //Espera a que los hilos terminen
     for (int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
-
-    printf("Fibonacci(%d) = %d\n", fiboAcalcular, array[fiboAcalcular]);
-
-    for (int i = 0; i < 30; i++) {
+    //Elimina los mutexes inicializados
+    for (int i = 0; i <= fiboAcalcular; i++) {
         pthread_mutex_destroy(&arrayMutex[i]);
     }
-
+    printf("Fibonacci(%d) = %d\n", fiboAcalcular, array[fiboAcalcular]);
     return 0;
 }
